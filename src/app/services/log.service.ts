cff59c9203d1fdb10c9ea26832e73edd40005b1a
Christian Bourlier <chrisbourlier@hotmail.com>
2018-10-16 15:52:53 -0600

    utilized local storage for persisting data

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -29,7 +29,14 @@ export class LogService {
   }
 
   getLogs(): Observable<Log[]> {
-    return of(this.logs)
+    if(localStorage.getItem('logs') === null) {
+      this.logs = []
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs'))
+    }
+    return of(this.logs.sort((a,b) => {
+      return b.date = a.date
+    }))
   }
 
   setFormLog(log: Log) {
@@ -38,6 +45,9 @@ export class LogService {
 
   addLog(log: Log) {
     this.logs.unshift(log)
+
+    //add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs))
   }
 
   updateLog(log: Log) {
@@ -47,6 +57,8 @@ export class LogService {
       }
     })
     this.logs.unshift(log)
+    localStorage.setItem('logs', JSON.stringify(this.logs))
+
   }
 
   deleteLog(log: Log) {
@@ -55,6 +67,8 @@ export class LogService {
         this.logs.splice(index, 1)
       }
     })
+    localStorage.setItem('logs', JSON.stringify(this.logs))
+
   }
   clearState() {
     this.stateSource.next(true)
